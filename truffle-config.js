0d184d96b8ab8d3507838cd2610e669b5115a112
Daniel Mallcott <daniel.mallcott@gmail.com>
2021-09-16 13:58:28 +0200

    Pushing to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,20 @@
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
     develop: {
       port: 9545
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/efe263b4367b4828aedbdc9bc0f1e759`),
+        network_id: 4,       
+        gas: 4500000,        
+        gasPrice: 10000000000
+    },
   },
   
   // Configure your compilers
